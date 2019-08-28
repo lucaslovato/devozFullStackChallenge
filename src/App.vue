7ac2ee214fdb794b0910ab2698e5930a4b7acf6c
Lucas Lovato <lucas.lovato@gmail.com>
2019-08-28 20:50:39 -0300

    view updates and server started

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,36 @@
 // App.vue
 
 <template>
-  <div>
-    <router-view></router-view>
+  <div class="container">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>  
+        <li class="nav-item">
+          <router-link to="/create" class="nav-link">Create Post</router-link>
+        </li>  
+        <li class="nav-item">
+          <router-link to="/posts" class="nav-link">Posts</router-link>
+        </li>
+      </ul>
+    </nav><br />
+    <transition name="fade">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
-<script>
-import HomeComponent from './components/HomeComponent.vue'
+<style>
+  .fade-enter-active, .fade-leave-active{
+    transition: opacity .5s;
+  }
+  .fade-enter, .fade-leave-active{
+    opacity: 0;
+  }
+</style>
 
+<script>
 export default {
-  name: 'app',
 }
 </script>
